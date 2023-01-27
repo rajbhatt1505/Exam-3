d0e689386fe1bc06f54f84459386e448fd40d367
rajbhatt1505 <rb794095@gmail.com>
2023-01-27 14:31:46 +0530

    origin

--- Angular/src/app/component/user/user.component.ts
+++ Angular/src/app/component/user/user.component.ts
@@ -56,11 +56,7 @@ export class UserComponent implements OnInit {
 
       
     }
-    if(this.auth.getToken()){
-      this.router.navigate(['main/home']);
-    }else{
-      this.router.navigate([''])
-    }
+ 
   }
 
   get Profile() {
